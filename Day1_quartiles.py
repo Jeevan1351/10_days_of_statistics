7d68cf57ce72182c3efdf8afc48e30ae7a1dc927
Jeevan1351 <vijayeshjeevan.cs19@bmsce.ac.in>
2021-06-03 12:23:31 +0530

    Working Code

--- Day1_quartiles.py
+++ Day1_quartiles.py
@@ -3,17 +3,17 @@ def quartiles(n, arr):
     # print(arr)
     q1, q2, q3 = 0, 0, 0
     if n % 2 == 0:
-        q2 = (arr[n//2]+arr[n//2-1])/2
-        if (n/2) % 2 != 0:
-            q1 = arr[n//4]
-            q3 = arr[(3*n)//4]
+        q2 = (arr[n // 2] + arr[n // 2 - 1]) / 2
+        if (n / 2) % 2 != 0:
+            q1 = arr[n // 4]
+            q3 = arr[(3 * n) // 4]
         else:
-            q1 = (arr[n//4]+arr[n//4-1])/2
-            q3 = (arr[(3*n)//4]+arr[(3*n)//4-1])/2
+            q1 = (arr[n // 4] + arr[n // 4 - 1]) / 2
+            q3 = (arr[(3 * n) // 4] + arr[(3 * n) // 4 - 1]) / 2
     else:
-        q2 = arr[n//2]
-        q1 = (arr[n//4]+arr[n//4-1])/2
-        q3 = (arr[(3*n)//4]+arr[(3*n)//4+1])/2
+        q2 = arr[n // 2]
+        q1 = (arr[n // 4] + arr[n // 4 - 1]) / 2
+        q3 = (arr[(3 * n) // 4] + arr[(3 * n) // 4 + 1]) / 2
 
     print(int(q1))
     print(int(q2))
@@ -24,5 +24,5 @@ n = int(input())
 values = input().split()
 numbers = []
 for i in values:
-    numbers.append(int(i))
+    numbers.append(int(i))  # input values and appended them to a list
 quartiles(n, numbers)
